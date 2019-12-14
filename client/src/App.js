aa47987028448335bbbc4c2f15e3aa73400bf645
Gunnar R <gunnarrosenberg@gmail.com>
2019-12-13 21:21:39 -0800

    updated react routes + components

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import {
-  HashRouter as Router, 
-  Route
-} from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom';
 import PostList from './components/PostList';
 import NewPost from './components/NewPost';
+import About from './components/About';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
+
+
+          <li><Link to={'/about'} className="nav-link">About</Link></li>
+          <li><Link to={'/'} className="nav-link">Home</Link></li>
+          <li><Link to={'/posts/new'} className="nav-link">New Post</Link></li>
+
         <Route exact path="/" component={PostList} />
+        <Route exact path="/about" component={About} />
         <Route exact path="/posts/new" component={NewPost} />
+
       </div>
     </Router>
   );
